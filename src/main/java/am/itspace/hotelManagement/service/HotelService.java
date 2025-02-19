3da4deb9cc35c94941c38d8c11125617f62555bf
Alik Sargsyan <alik.sargsyan@coderepablic.am>
2025-02-20 01:37:54 +0400

    added some methods

--- src/main/java/am/itspace/hotelManagement/service/HotelService.java
+++ src/main/java/am/itspace/hotelManagement/service/HotelService.java
@@ -3,6 +3,8 @@ package am.itspace.hotelManagement.service;
 import am.itspace.hotelManagement.dto.request.HotelRequest;
 import am.itspace.hotelManagement.dto.response.HotelResponse;
 import am.itspace.hotelManagement.model.Hotel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,7 +12,8 @@ import java.util.Optional;
 public interface HotelService {
   HotelResponse addHotel(HotelRequest hotelRequest);
   Optional<HotelResponse> getHotelById(int hotelId);
-  List<HotelResponse> getAllHotels();
+  Page<HotelResponse> getAllHotels(int page, int size);
+  List<HotelResponse> filterHotel(Boolean isFreeWiFi, Boolean isSwimmingPool, Boolean isParking, Boolean isFitnessCenter);
   void deleteHotel(int hotelId);
   Hotel updateHotel(HotelRequest hotelRequest);
 }
