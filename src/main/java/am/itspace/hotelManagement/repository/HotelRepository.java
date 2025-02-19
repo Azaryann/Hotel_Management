04d044ea1be2c92eddfff7461715f8c4903f95c3
Alik Sargsyan <alik.sargsyan@coderepablic.am>
2025-02-20 01:38:45 +0400

    extended Jpa executor

--- src/main/java/am/itspace/hotelManagement/repository/HotelRepository.java
+++ src/main/java/am/itspace/hotelManagement/repository/HotelRepository.java
@@ -2,6 +2,8 @@ package am.itspace.hotelManagement.repository;
 
 import am.itspace.hotelManagement.model.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface HotelRepository extends JpaRepository<Hotel, Integer> {
+public interface HotelRepository extends JpaRepository<Hotel, Integer>, JpaSpecificationExecutor<Hotel> {
+
 }
